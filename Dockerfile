ea5a2490da07629f3f70f9bf3a1fd9dc4b391d0c
tnhnclskn <tunahancaliskan@yahoo.com>
2018-07-08 13:40:30 +0300

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM php:7.1-fpm
+
+MAINTAINER Tunahan ÇALIŞKAN <mail@tunahancaliskan.com.tr>
+
+RUN apt-get update
+RUN apt-get install -y libmcrypt-dev libicu-dev mysql-client
+RUN docker-php-ext-install pdo_mysql
+RUN docker-php-ext-install iconv
+RUN docker-php-ext-install mcrypt
+RUN docker-php-ext-install intl
+RUN docker-php-ext-install opcache
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+WORKDIR /var/www/html
+
+CMD ["php-fpm"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+isCommand() {
+  for cmd in \
+    "about" \
+    "archive" \
+    "browse" \
+    "clear-cache" \
+    "clearcache" \
+    "config" \
+    "create-project" \
+    "depends" \
+    "diagnose" \
+    "dump-autoload" \
+    "dumpautoload" \
+    "exec" \
+    "global" \
+    "help" \
+    "home" \
+    "info" \
+    "init" \
+    "install" \
+    "licenses" \
+    "list" \
+    "outdated" \
+    "prohibits" \
+    "remove" \
+    "require" \
+    "run-script" \
+    "search" \
+    "self-update" \
+    "selfupdate" \
+    "show" \
+    "status" \
+    "suggests" \
+    "update" \
+    "validate" \
+    "why" \
+    "why-not"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+if [ "$1" = 'php-fpm' ]; then
+  exec "$@"
+else
+  exec php "$@"
+fi
